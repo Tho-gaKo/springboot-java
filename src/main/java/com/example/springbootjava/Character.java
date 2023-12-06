097384313ec1c1b830c77991aa668179263d7766
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-12-06 16:30:45 +0100

    commit

---
+++ src/main/java/com/example/springbootjava/Character.java
@@ -0,0 +1,4 @@
+package com.example.springbootjava;
+
+public record Character(String id, String name, int age, String profession) {
+}
---
+++ src/main/java/com/example/springbootjava/MongoInterface.java
@@ -0,0 +1,6 @@
+package com.example.springbootjava;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MongoInterface extends MongoRepository<Character-Record, String> {
+}
